01cd0112384c3d8c7d475544222b32e32e26e26d
Kaleb Belnap <10929271@my.uvu.edu>
2020-10-05 22:51:53 -0600

    Added collisions to animals and cookie

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,12 +7,11 @@ public class DestroyOutOfBounds : MonoBehaviour
     public float topBounds = 30f;
     public float lowerBounds = -10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+void Awake()
+{
+    Time.timeScale =1;
+}
     // Update is called once per frame
     void Update()
     {
@@ -23,7 +22,9 @@ public class DestroyOutOfBounds : MonoBehaviour
         // Destroy any object entering the lowerbounds
         else if(transform.position.z < lowerBounds)
         {
+            Debug.Log("GAME OVER!");
             Destroy(gameObject);
+            Time.timeScale = 0;
         }
     }
 }
