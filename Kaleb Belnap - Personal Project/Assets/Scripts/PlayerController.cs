22d4943ed1ca0cd6a02e223031dfec0bee3dc038
Kaleb Belnap <10929271@my.uvu.edu>
2020-11-30 14:55:50 -0700

    Personal Project - Player Control
    rotation that follows mouse position

---
+++ Kaleb Belnap - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform Player;
+    public float playerTurnSpeed;
+    float playerAngle;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePlayer();
+    }
+
+    void RotatePlayer()
+    {
+        playerAngle += Input.GetAxis("Mouse Z") * playerTurnSpeed * -Time.deltaTime;
+        playerAngle = Mathf.Clamp(playerAngle, 0, 360);
+        Player.localRotation = Quaternion.AngleAxis(playerAngle, Vector3.up); // Rotate only on Y
+    }
+}
