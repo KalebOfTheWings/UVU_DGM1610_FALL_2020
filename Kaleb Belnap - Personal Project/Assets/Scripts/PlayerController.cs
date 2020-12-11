06c46823701b20d904b07fa3dba531349e244cdc
Kaleb Belnap <10929271@my.uvu.edu>
2020-12-11 15:36:20 -0700

    Personal Project Updates

--- Kaleb Belnap - Personal Project/Assets/Scripts/PlayerController.cs
+++ Kaleb Belnap - Personal Project/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform Player;
-    public float playerTurnSpeed;
-    float playerAngle;
+    public GameObject projectilePrefab;
+    public GameObject player;
+    public GameObject shooter;
     
-    // Update is called once per frame
-    void Update()
+    
+
+    // Start is called before the first frame update
+    void Start()
     {
-        RotatePlayer();
+        
     }
 
-    void RotatePlayer()
+    void Update() 
     {
-        playerAngle += Input.GetAxis("Mouse Z") * playerTurnSpeed * -Time.deltaTime;
-        playerAngle = Mathf.Clamp(playerAngle, 0, 360);
-        Player.localRotation = Quaternion.AngleAxis(playerAngle, Vector3.up); // Rotate only on Y
+        FaceMouse();
+        Blaster();
+       
+    }
+
+
+    void FaceMouse() // Player faces the mouse
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void Blaster() // Controls what the blaster shoots
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, shooter.transform.position, shooter.transform.rotation);
+        }
     }
 }
