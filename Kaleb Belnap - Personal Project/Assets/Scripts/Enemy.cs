84eb6885ad0908c444a3c8e467f6687a69373bb4
Kaleb Belnap <10929271@my.uvu.edu>
2020-12-11 17:17:14 -0700

    Personal Project more updates

---
+++ Kaleb Belnap - Personal Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Target the Player
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FacePlayer();
+        MoveForward();
+    }
+
+    void FacePlayer()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+    }
+
+    void MoveForward()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+}
+
---
+++ Kaleb Belnap - Personal Project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] alienPrefabs;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int alienIndex = Random.Range(0, 3);
+
+            Instantiate(alienPrefabs[alienIndex], new Vector3(0, 30, 3), alienPrefabs[alienIndex].transform.rotation);
+        }
+    }
+}
