a10dc056965f1b0b5c31da5164a797c169693f30
Kaleb Belnap <10929271@my.uvu.edu>
2021-04-06 14:27:48 -0600

    Organized Work

---
+++ Class Work/Kaleb Belnap - Personal Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 10;
+    public GameObject player;
+    public float lowerBound = 6;
+    public bool gameOver;
+    private GameManager gameManager;
+    
+   
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        target = GameObject.FindGameObjectWithTag("Player").transform; //Target the Player
+    }
+
+    public void Update() // Calls all the actions below
+    {
+        FacePlayer(); // Enemy faces player
+        MoveForward(); // Enemy moves toward player at different rates
+        
+        if (transform.position.y < lowerBound) // Enemies get destroyed at y position of player
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+    }
+
+    void FacePlayer() // When enemy spawns it will face the player.
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+    }
+
+    void MoveForward() // Controls the speed of the enemy.
+    {
+        if(gameManager.isGameActive)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
+
---
+++ Class Work/Kaleb Belnap - Personal Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip shootSound;
+    public static AudioClip crashSound;
+    static AudioSource audioSrc;
+
+    void Start() // Finds sound locations
+    {
+        crashSound = Resources.Load<AudioClip>("crashSound");
+        shootSound = Resources.Load<AudioClip>("shootSound");
+        audioSrc = GetComponent <AudioSource> ();
+    }
+
+    public static void playSound() // Plays the crash sound
+    {
+        audioSrc.PlayOneShot(crashSound, 0.1f);
+    }
+
+    public static void playSound2() // Plays the shoot sound
+    {
+        audioSrc.PlayOneShot(shootSound, 1.0f);
+    }
+}
